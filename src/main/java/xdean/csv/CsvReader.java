df5ca95a6b78bbf06a46cdd68616eba13447b438
XDean <373216024@qq.com>
2018-03-19 10:46:50 +0800

    add default read(Reader)

--- src/main/java/xdean/csv/CsvReader.java
+++ src/main/java/xdean/csv/CsvReader.java
@@ -3,19 +3,25 @@ package xdean.csv;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 
 import io.reactivex.Flowable;
 
 public interface CsvReader {
 
-  CsvResult read(Flowable<String> lines);
-
   CsvReader splitor(String splitor);
 
   CsvReader addColumn(CsvColumn<?> column);
 
+  CsvReader addColumnsFromBean(Class<?> bean);
+
+  CsvResult read(Flowable<String> lines);
+
   default CsvResult read(InputStream stream) {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+    return read(new InputStreamReader(stream));
+  }
+  default CsvResult read(Reader stream) {
+    BufferedReader reader = new BufferedReader(stream);
     return read(Flowable.generate(e -> {
       String line = reader.readLine();
       if (line == null) {
